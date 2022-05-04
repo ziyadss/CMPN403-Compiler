212f3c0026143f2daf5dcfb4bebccf73df0587a8
Ziyad <ziyad.ss@hotmail.com>
2022-05-04 22:30:11 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-rm -r build bin
-mkdir build bin
-lex -o build/lex.yy.c src/lexer.l
-yacc -o build/y.tab.c -d src/parser.y
-gcc -Wall -Wextra -o bin/compiler.exe build/lex.yy.c build/y.tab.c
-echo "Starting..."
-./bin/compiler.exe tests/source.c
-echo "Done!"
