806c69a535e6fca2b804b932c4a6aad9e1724aae
Ziyad <ziyad.ss@hotmail.com>
2022-05-26 18:39:14 +0200

    Update playground.c
    Formatting
    changed some function return types
    added allocation assertions
    removed a TODO and edited another

--- src/playground.c
+++ src/playground.c
@@ -1,5 +1,7 @@
 #define ST_ARRAY_SIZE 1
 
+#include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -9,30 +11,33 @@ enum SyntaxError
     USED_IDENTIFIER
 };
 
-enum KIND {
-	FUNCT,
-	PAR,
-	VAR
+enum KIND
+{
+    FUNCT,
+    PAR,
+    VAR
 };
 
-enum MODIFIER {
-	AUTO,
-	CONST,
-	STATIC,
-	UNSIGNED,
-	NONE
+enum MODIFIER
+{
+    AUTO,
+    CONST,
+    STATIC,
+    UNSIGNED,
+    NONE
 };
 
-enum TYPE {
-	BOOL,
-	CHAR,
-	DOUBLE,
-	FLOAT,
-	INT,
-	LONG,
-	SHORT,
-	STRING,
-	VOID
+enum TYPE
+{
+    BOOL,
+    CHAR,
+    DOUBLE,
+    FLOAT,
+    INT,
+    LONG,
+    SHORT,
+    STRING,
+    VOID
 };
 
 struct SymbolTable
@@ -40,43 +45,44 @@ struct SymbolTable
     struct SymbolTableEntry
     {
         char *identifier;
-		//KIND kind;
-		//type
-		//MODIFIER modifier;
+        // KIND kind;
+        // type
+        // MODIFIER modifier;
 
         struct SymbolTableEntry *next;
-    } *buckets[ST_ARRAY_SIZE];
+    } * buckets[ST_ARRAY_SIZE];
 
     struct SymbolTable *parent;
 };
 
 unsigned int hash(char *string)
 {
-    static unsigned int counter = 0;
-    return counter++ % ST_ARRAY_SIZE;
+    return strlen(string) % ST_ARRAY_SIZE;
 }
 
-_Bool search(struct SymbolTableEntry *node, char *identifier)
+struct SymbolTableEntry *search(struct SymbolTableEntry *node, char *identifier)
 {
     while (node != NULL)
     {
         if (strcmp(node->identifier, identifier) == 0)
-            return 1;
+            return node;
         node = node->next;
     }
 
-    return 0;
+    return NULL;
 }
 
-int insert(struct SymbolTable *table, char *identifier)
+enum SyntaxError insert(struct SymbolTable *table, char *identifier)
 {
     unsigned int bucket = hash(identifier);
     struct SymbolTableEntry *head = table->buckets[bucket];
 
-    if (search(head, identifier))
+    if (search(head, identifier) != NULL)
         return USED_IDENTIFIER;
 
     struct SymbolTableEntry *entry = malloc(sizeof(*entry));
+    assert(entry != NULL);
+
     entry->identifier = identifier;
     entry->next = head;
 
@@ -90,47 +96,46 @@ static struct SymbolTable *current_scope;
 struct SymbolTable *create_table()
 {
     struct SymbolTable *table = malloc(sizeof(*table));
+    assert(table != NULL);
+
     table->parent = NULL;
     for (int i = 0; i < ST_ARRAY_SIZE; i++)
         table->buckets[i] = NULL;
+
     return table;
 }
 
-int destroy_table(struct SymbolTable *table)
+void destroy_table(struct SymbolTable *table)
 {
-	struct SymbolTableEntry** entries = table->buckets;
+    struct SymbolTableEntry **entries = table->buckets;
 
-	for (int i = 0; i < ST_ARRAY_SIZE; ++i)
-	{
-		while (entries[i]->next != NULL)
-		{
-			struct SymbolTableEntry** tmp = entries;
-			entries[i] = entries[i]->next;
-			free(tmp);
-		}
-		free(entries[i]);
-	}
-
+    for (int i = 0; i < ST_ARRAY_SIZE; ++i)
+    {
+        while (entries[i]->next != NULL)
+        {
+            struct SymbolTableEntry **tmp = entries;
+            entries[i] = entries[i]->next;
+            free(tmp);
+        }
+        free(entries[i]);
+    }
 
     free(table);
 }
 
-int scope_down()
+void scope_down()
 {
     struct SymbolTable *new_scope = create_table();
     new_scope->parent = current_scope;
     current_scope = new_scope;
 }
 
-int scope_up()
+void scope_up()
 {
     struct SymbolTable *old_scope = current_scope;
     current_scope = old_scope->parent;
     destroy_table(old_scope);
 }
-
-#include <stdio.h>
-#include <assert.h>
 
 int main()
 {
@@ -160,9 +165,7 @@ int main()
 // TODO: A search function that searches in the table, if not found in its parent, etc etc. It calls the currently
 //  implemented search function for the one correct bucket in each table.
 
-// TODO: A hash function that produces the same hash for identical strings. Can do strlen for now??
+// TODO: A hash function that produces the same hash for identical strings.
 //  Note that ST_ARRAY_SIZE is currently set to 1 so that there is only one bucket.
 
 // TODO: SymbolTableEntry properties
-
-// TODO: destroy_table logic
