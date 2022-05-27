47c38b74e43774b70229e4210068d20592c61c8e
Khaled Ibrahim <khaledashrafabdulrahman@gmail.com>
2022-05-27 14:39:01 +0200

    untested stuff lol
    woopsie

--- src/symbol_table.c
+++ src/symbol_table.c
@@ -1,5 +1,5 @@
 #define ST_ARRAY_SIZE 1
-
+#define HASH_SEED 0
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,85 +9,101 @@
 
 // template el ghalaba - abdo
 #define MAP_KV(T, Name)       \
-    typedef struct Map_##Name \
-    {                         \
-        char *key;            \
-        T value;              \
-    } Map_##Name##_El, *Map_##Name
+	typedef struct Map_##Name \
+{                         \
+	char *key;            \
+	T value;              \
+} Map_##Name##_El, *Map_##Name
+
+enum TYPE {
+	INT
+		/*...*/
+};
 
 struct SymbolTableEntry
 {
-    char *identifier;
-    /* ... */
+	char *identifier;
+	enum TYPE* TYPES;	 
+	_Bool is_init;
+	_Bool is_used;
+	_Bool is_func;
+	_Bool is_const;
 
-    struct SymbolTableEntry *next;
+	struct SymbolTableEntry *next;
 };
+
 
 struct SymbolTable
 {
-    struct SymbolTableEntry *buckets[ST_ARRAY_SIZE];
+	struct SymbolTableEntry *buckets[ST_ARRAY_SIZE];
 
-    struct SymbolTable *parent;
+	struct SymbolTable *parent;
 };
 
 unsigned int hash(char *string)
 {
-    return strlen(string) % ST_ARRAY_SIZE;
+	int hash = stbds_hash_string(string, HASH_SEED);
+	return hash % ST_ARRAY_SIZE;
 }
 
 struct SymbolTableEntry *search_bucket(struct SymbolTableEntry *node, char *identifier)
 {
-    while (node != NULL)
-    {
-        if (strcmp(node->identifier, identifier) == 0)
-            return node;
-        node = node->next;
-    }
+	while (node != NULL)
+	{
+		if (strcmp(node->identifier, identifier) == 0)
+			return node;
+		node = node->next;
+	}
 
-    return NULL;
+	return NULL;
 }
 
 struct SymbolTableEntry *search_tables(struct SymbolTable *table, char *identifier)
 {
-    unsigned int bucket = hash(identifier);
+	unsigned int bucket = hash(identifier);
 
-    while (table != NULL)
-    {
-        struct SymbolTableEntry *entry = search_bucket(table->buckets[bucket], identifier);
-        if (entry != NULL)
-            return entry;
-        table = table->parent;
-    }
+	while (table != NULL)
+	{
+		struct SymbolTableEntry *entry = search_bucket(table->buckets[bucket], identifier);
+		if (entry != NULL)
+			return entry;
+		table = table->parent;
+	}
 
-    return NULL;
+	return NULL;
 }
 
 struct SymbolTable *create_table()
 {
-    struct SymbolTable *table = malloc(sizeof(*table));
-    assert(table != NULL);
+	struct SymbolTable *table = malloc(sizeof(*table));
+	assert(table != NULL);
 
-    table->parent = NULL;
-    for (int i = 0; i < ST_ARRAY_SIZE; i++)
-        table->buckets[i] = NULL;
+	table->parent = NULL;
+	for (int i = 0; i < ST_ARRAY_SIZE; i++)
+		table->buckets[i] = NULL;
 
-    return table;
+	return table;
 }
 
 void destroy_table(struct SymbolTable *table)
 {
-    for (int i = 0; i < ST_ARRAY_SIZE; i++)
-    {
-        struct SymbolTableEntry *head = table->buckets[i];
-        while (head != NULL)
-        {
-            struct SymbolTableEntry *next = head->next;
-            free(head);
-            head = next;
-        }
-    }
-    
-    free(table);
+	for (int i = 0; i < ST_ARRAY_SIZE; i++)
+	{
+		struct SymbolTableEntry *head = table->buckets[i];
+		while (head != NULL)
+		{
+			struct SymbolTableEntry *next = head->next;
+			free(head);
+			head = next;
+		}
+	}
+
+	free(table);
+}
+
+int main()
+{
+	return 0;
 }
 
 // TODO: A hash function that produces the same hash for identical strings.
