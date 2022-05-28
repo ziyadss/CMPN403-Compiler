c79f8d6f421a955f3de4f65b70ecfe49a8d1dc2a
Ziyad <ziyad.ss@hotmail.com>
2022-05-28 13:10:24 +0200

    Update ziyad.c

--- tests/sources/ziyad.c
+++ tests/sources/ziyad.c
@@ -8,3 +8,26 @@ void main()
   int a = sum(10, 20);
   a = sum(a, a);
 }
+
+/*
+sum: 
+POP retadr
+POP y
+POP x
+ADD retval, x, y
+PUSH retadr
+RET
+
+main: 
+POP retadr
+PUSH 10
+PUSH 20
+CALL sum
+MOV a, retval
+PUSH a
+PUSH a
+CALL sum
+MOV a, retval
+PUSH retadr
+RET
+*/
