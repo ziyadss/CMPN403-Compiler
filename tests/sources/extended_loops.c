6a240d7659ed4592135068768f91cef9334b71b4
Ziyad <ziyad.ss@hotmail.com>
2022-05-28 23:05:40 +0200

    correcting continue label + formatting

---
+++ tests/sources/extended_loops.c
@@ -0,0 +1,20 @@
+int main()
+{
+    int x = 1;
+    int y = 2;
+    // int x = 1, y = 2;
+    while (x)
+    {
+        int a = 10;
+        if (y == 3)
+        {
+            int z = 3;
+            break;
+        }
+        else
+        {
+            continue;
+        }
+        int b = 12;
+    }
+}
